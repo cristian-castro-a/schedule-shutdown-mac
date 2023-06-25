f167d9bb4d5df2c8594ec2040a5f09b7b8f68b13
cristian-castro-a <cr.castro.alvarez@gmail.com>
2023-06-25 15:09:11 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# schedule-shut-down-mac
-Script to put your computer to sleep
+# Schedule Your Mac to Shutdown
+Do you use your Mac to watch Netflix and would you like to schedule a shutdown time for when you fall asleep? This repo is for you!
+
+## Installation
+This repo uses Python 3.9. To install a Conda environment please use:
+```bash
+conda create --name myenv --file requirements.txt
+```
+
+## Use
---
+++ main.py
@@ -0,0 +1,35 @@
+import yaml
+from pathlib import Path
+from typing import Dict
+from yaml.loader import BaseLoader
+import os
+
+CONFIG_FILE = 'config.yaml'
+
+
+def load_yaml(config_file: Path) -> Dict:
+    with open(config_file, 'r') as stream:
+        try:
+            dict = yaml.load(stream=stream, Loader=BaseLoader)
+            return dict
+        except FileNotFoundError as e:
+            print('Config.yaml not found')
+
+
+def run(config_file: Path) -> None:
+    config = load_yaml(config_file=config_file)
+    days_of_the_week = ''.join([config['Nomenclature'].get(i) for i in config['Days']])
+    time_of_the_day = f"{config['Time'][0]}:00"
+
+    os.system(f"sudo pmset repeat shutdown {days_of_the_week} {time_of_the_day}")
+
+
+if __name__ == '__main__':
+    """
+    Sets a schedule for your Mac computer to shutdown
+    
+    inputs:
+        - config.yaml: States the days and time at which you want to shutdown your Mac
+    """
+    config_file = Path.cwd() / CONFIG_FILE
+    run(config_file=config_file)
